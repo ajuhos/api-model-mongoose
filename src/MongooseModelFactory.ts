921140d72dc81ee99211f3dfc7bc53e759c975e7
Juhos Ádám <ajuhos@logiscool.com>
2018-03-18 20:10:07 +0100

    Generated typed schemas from Mongoose schemas
    This commit adds support for automatic schema conversion from the Mongoose schemas to the internal typed schema (simpl-schema format) of API Core. Currently only simple fields and objects are supported. Work required: Arrays, ObjectIDs and other special types of Mongoose schemas.

--- src/MongooseModelFactory.ts
+++ src/MongooseModelFactory.ts
@@ -2,24 +2,42 @@ import {ApiEdgeSchema} from "api-core";
 import * as mongoose from "mongoose";
 import {MongooseModelEdge} from "./MongooseModelEdge";
 
+function mapFieldToSimple(field: any) {
+    //TODO
+    return field
+}
+
+function mapSchema(schema: any, publicSchema: any) {
+    const publicFields = Object.keys(publicSchema);
+    const output: { [key: string]: any } = {};
+    for(let field of publicFields) {
+        //TODO. Support deep fields
+        const schemaField = schema[field];
+        if(schemaField) {
+            output[field] = mapFieldToSimple(schemaField)
+        }
+    }
+    return output
+}
+
 export class MongooseModelFactory {
 
-    static createModel(name: string, plural: string, scheme: any, publicScheme: any, keyField: string = MongooseModelEdge.defaultKeyField) {
+    static createModel(name: string, plural: string, schema: any, publicSchema: any, keyField: string = MongooseModelEdge.defaultKeyField) {
         let model = new MongooseModelEdge<mongoose.Document>();
         model.name = name;
         model.pluralName = plural;
-        model.provider = mongoose.model<mongoose.Document>(name, new mongoose.Schema(scheme));
-        model.schema = new ApiEdgeSchema(publicScheme);
+        model.provider = mongoose.model<mongoose.Document>(name, new mongoose.Schema(schema));
+        model.schema = new ApiEdgeSchema(publicSchema, mapSchema(schema, publicSchema));
         model.keyField = keyField;
         return model
     }
 
-    static createModelOnConnection(connection: mongoose.Connection, name: string, plural: string, scheme: any, publicScheme: any, keyField: string = MongooseModelEdge.defaultKeyField) {
+    static createModelOnConnection(connection: mongoose.Connection, name: string, plural: string, schema: any, publicSchema: any, keyField: string = MongooseModelEdge.defaultKeyField) {
         let model = new MongooseModelEdge<mongoose.Document>();
         model.name = name;
         model.pluralName = plural;
-        model.provider = connection.model<mongoose.Document>(name, new mongoose.Schema(scheme));
-        model.schema = new ApiEdgeSchema(publicScheme);
+        model.provider = connection.model<mongoose.Document>(name, new mongoose.Schema(schema));
+        model.schema = new ApiEdgeSchema(publicSchema, mapSchema(schema, publicSchema));
         model.keyField = keyField;
         return model
     }
